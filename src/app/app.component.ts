99b008ded5709ec665c21846bf42630e6a1b7ca6
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-05 19:37:20 +0000

    App components import productComponent and cartComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from "./components/products/products.component";
+import { CartComponent } from "./components/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
