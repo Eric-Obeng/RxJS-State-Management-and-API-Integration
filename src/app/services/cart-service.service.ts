7b657de423c36f007f55c8254c73b2fccb3902c4
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-05 23:44:06 +0000

    cart service updated to add localstorage

--- src/app/services/cart-service.service.ts
+++ src/app/services/cart-service.service.ts
@@ -6,16 +6,28 @@ import { Product } from '../interface/product';
   providedIn: 'root',
 })
 export class CartService {
-  cartItems$ = new BehaviorSubject<Product[]>([]);
+  cartItems$ = new BehaviorSubject<Product[]>(this.loadCartItems());
   cartItems = this.cartItems$.asObservable();
 
-  totalItems$ = new BehaviorSubject<number>(0);
+  totalItems$ = new BehaviorSubject<number>(
+    this.calculateTotalItems(this.cartItems$.getValue())
+  );
   totalItems: Observable<number> = this.totalItems$.asObservable();
 
   constructor() {
     this.cartItems.subscribe((items) => {
-      this.totalItems$.next(items.length);
+      this.totalItems$.next(this.calculateTotalItems(items));
+      this.saveCartItems(items);
     });
+  }
+
+  private loadCartItems(): Product[] {
+    const storedItems = localStorage.getItem('cart');
+    return storedItems ? JSON.parse(storedItems) : [];
+  }
+
+  private saveCartItems(items: Product[]): void {
+    localStorage.setItem('cart', JSON.stringify(items));
   }
 
   getCartItems(): Observable<Product[]> {
@@ -24,25 +36,29 @@ export class CartService {
 
   addToCart(product: Product) {
     const currentItem = this.cartItems$.getValue();
-    // const existingItem = currentItem.findIndex(
-    //   (item) => item.id === product.id
-    // );
+    const existingItem = currentItem.find((item) => item.id === product.id);
 
-    // if (existingItem) {
-    //   currentItem[existingItem].quantity += 1;
-    // } else {
-    //   product.quantity = 1;
-    //   currentItem.push(product);
-    // }
-    this.cartItems$.next([...this.cartItems$.value, product]);
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      product.addToCart = true;
+      product.quantity = 1;
+      currentItem.push(product);
+    }
+    this.cartItems$.next(currentItem);
   }
 
   removeFromCart(productId: number) {
     const currentItem = this.cartItems$.getValue();
     this.cartItems$.next(currentItem.filter((item) => item.id !== productId));
+    console.log('Deleting product with ID', productId);
   }
 
   clearCart() {
     this.cartItems$.next([]);
+  }
+
+  calculateTotalItems(items: Product[]): number {
+    return items.reduce((total, item) => total + item.quantity, 0);
   }
 }
