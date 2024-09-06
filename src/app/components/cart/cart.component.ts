807f5dfd5d9970090e3c96d5d67359f5827b5e55
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-06 09:25:15 +0000

    added clear cart button and functionalities

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart-service.service';
-import { Observable } from 'rxjs';
+import { delay, Observable, tap } from 'rxjs';
 import { Product } from '../../interface/product';
 import { CommonModule } from '@angular/common';
 
@@ -14,20 +14,37 @@ import { CommonModule } from '@angular/common';
 export class CartComponent {
   cartItems: Product[] = [];
   totalItems: number = 0;
+  showCart: boolean = false;
+
+  showCartAfterDelay() {
+    setTimeout(() => {
+      this.showCart = true;
+    }, 6000);
+  }
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cartService.getCartItems().subscribe({
-      next: (items) => {
-        this.cartItems = items;
-        this.totalItems = this.cartService.calculateTotalItems(items);
-      },
-      error: (error) => console.error('Error fetching cart items:', error),
-    });
+    this.cartService
+      .getCartItems()
+      .pipe(
+        // delay(5000),
+        tap(() => this.showCartAfterDelay())
+      )
+      .subscribe({
+        next: (items) => {
+          this.cartItems = items;
+          this.totalItems = this.cartService.calculateTotalItems(items);
+        },
+        error: (error) => console.error('Error fetching cart items:', error),
+      });
   }
 
   removeFromCart(productId: number) {
     this.cartService.removeFromCart(productId);
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
   }
 }
